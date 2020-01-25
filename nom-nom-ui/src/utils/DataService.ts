d6018dc76e43b14bec9cddc62dea67dbfc3badb0
micahhauge <micahhauge@gmail.com>
2020-01-25 02:23:36 -0600

    stub out functions

--- nom-nom-ui/src/utils/DataService.ts
+++ nom-nom-ui/src/utils/DataService.ts
@@ -1,10 +1,26 @@
 import { db } from './firebase';
 
-interface Host {
-    name: string;
-    number: string
-    location: string
+interface Food {
+    type: string;
+    servings: string;
+    location: { 
+        Latitude: number,
+        Longitude: number
+    };
+    prepDate: Date;
+    allergens: string[];
 }
+
+interface User {
+    name: string;
+    number: string;
+    email: string;
+}
+
+async function getFoodById(id: string) {}
+async function getUserById(id: string) {}
+async function getAllFood() {}
+async function getAllUsers() {}
 
 async function getHostById(id: string): Promise<Host>{
     const hostSnapshot = await db.doc(`/host/${id}`).get();
