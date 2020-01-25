1df9ce20e079fb0adff9cdbec59422ede6fe06a5
henna khan <hennakhan@gmail.com>
2020-01-25 16:28:38 -0600

    fix map view

--- nom-nom-ui/src/components/MapView.tsx
+++ nom-nom-ui/src/components/MapView.tsx
@@ -1,31 +1,46 @@
 import React, { Component } from 'react'
 import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
-import { LatLng, LatLngBounds } from 'leaflet';
+import { LatLng, LatLngBounds, latLng } from 'leaflet';
 
 type State = {
-  lat: number,
-  lng: number,
   zoom: number,
+  position: LatLng,
+}
+
+function getLocation(): Promise<Position> {
+  return new Promise((resolve, reject) => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(resolve, reject);
+    } else {
+      console.log("Geolocation is not supported by this browser.");
+      reject("Geolocation is not supported by this browser.");
+    }
+  });
 }
 
 export default class MapComponent extends Component<{}, State> {
   state = {
-    lat: 51.505,
-    lng: -0.09,
     zoom: 13,
+    position: new LatLng(36.1627, -86.7816)
   }
 
 
-  render() {
-    const position = new LatLng(36.174465, -86.767960);
+  async componentDidMount() {
+    const userLocation = await getLocation();
+    const position = new LatLng(userLocation.coords.latitude, userLocation.coords.longitude);
+    this.setState({
+      position 
+    });
+  }
 
+  render() {
     return (
-      <Map center={position} zoom={this.state.zoom}>
+      <Map center={this.state.position} zoom={this.state.zoom}>
         <TileLayer
           attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={position}>
+        <Marker position={this.state.position}>
           <Popup>
             A pretty CSS3 popup. <br /> Easily customizable.
           </Popup>
