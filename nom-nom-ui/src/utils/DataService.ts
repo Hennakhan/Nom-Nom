d22b89ded534e15a2a1554e4c2984cd20bb7f09a
prakash <sunnyrkg@gmail.com>
2020-01-25 02:54:23 -0600

    New data service functions

--- nom-nom-ui/src/utils/DataService.ts
+++ nom-nom-ui/src/utils/DataService.ts
@@ -3,7 +3,7 @@ import { db } from './firebase';
 interface Food {
     type: string;
     servings: string;
-    location: { 
+    location: {
         Latitude: number,
         Longitude: number
     };
@@ -17,15 +17,20 @@ interface User {
     email: string;
 }
 
-async function getFoodById(id: string) {}
+async function getFoodById(id: string) {
+  const foodSnap = db.doc(`/food/${id}`).get();
+  const foodData = foodSnap.data();
+  return foodData as Food; 
+
+}
 async function getUserById(id: string) {}
 async function getAllFood() {}
 async function getAllUsers() {}
 
-async function getHostById(id: string): Promise<Host>{
-    const hostSnapshot = await db.doc(`/host/${id}`).get();
-    const hostData = hostSnapshot.data();
-    return hostData as Host;
+async function getUserById(id: string): Promise<User>{
+    const userSnapshot = await db.doc(`/user/${id}`).get();
+    const userData = userSnapshot.data();
+    return userData as User;
 }
 
 export {
