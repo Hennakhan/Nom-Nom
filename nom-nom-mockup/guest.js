f6d590f6f94d94e0d9c324e6f759b823bcf26919
Richa Phulwani <rphulwani48@gmail.com>
2020-01-25 16:25:47 -0600

    guest

---
+++ nom-nom-mockup/guest.js
@@ -0,0 +1,10 @@
+function AddNewRecord()
+    {
+        // var grd = document.getElementById('gridcont');
+        document.getElementById("gridcont").style.gridTemplateRows += 1;
+        // var tbod=grd.rows[0].parentNode;
+        // var newRow=grd.rows[grd.rows.length - 1].cloneNode(true);
+        // tbod.appendChild(newRow);
+        return false;
+
+    }
