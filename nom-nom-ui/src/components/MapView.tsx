881901e60627113b0ffdd76851dcbca0ec8eb74c
micahhauge <micahhauge@gmail.com>
2020-01-26 02:57:09 -0600

    add map

--- nom-nom-ui/src/components/MapView.tsx
+++ nom-nom-ui/src/components/MapView.tsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
-import { LatLng, LatLngBounds, latLng } from 'leaflet';
+import { LatLng } from 'leaflet';
+import { getAllFood, Food } from '../utils/DataService';
 
 type State = {
   zoom: number,
   position: LatLng,
+  food: Food[]
 }
 
 function getLocation(): Promise<Position> {
@@ -18,22 +20,30 @@ function getLocation(): Promise<Position> {
   });
 }
 
-export default class MapComponent extends Component<{}, State> {
-  state = {
+export default class MapView extends Component<{}, State> {
+  state: State = {
     zoom: 13,
-    position: new LatLng(36.1627, -86.7816)
+    position: new LatLng(36.1627, -86.7816), 
+    food: []
   }
-
 
   async componentDidMount() {
     const userLocation = await getLocation();
     const position = new LatLng(userLocation.coords.latitude, userLocation.coords.longitude);
     this.setState({
-      position 
+      position
+    });
+
+    const foodData = await getAllFood();
+    this.setState({
+      food: foodData
     });
   }
 
   render() {
+    //  const dummyfoodData = [{ type: "chinese", servings: 4, location: { lat: 36.7, lon: -85.7 }, address: "123 Second Street Columbia TN" },
+    //  { type: "chinese", servings: 4, location: { lat: 34.8, lon: -86.9 }, address: "Not Specified, TN" }];
+
     return (
       <Map center={this.state.position} zoom={this.state.zoom}>
         <TileLayer
@@ -45,6 +55,15 @@ export default class MapComponent extends Component<{}, State> {
             A pretty CSS3 popup. <br /> Easily customizable.
           </Popup>
         </Marker>
+
+        {this.state.food.map(food => {
+          let pos = new LatLng(food.location.Latitude, food.location.Longitude);
+          return <Marker position={pos}>
+            <Popup>
+              {food.name}
+            </Popup>
+          </Marker>
+        })}
       </Map>
     )
   }
