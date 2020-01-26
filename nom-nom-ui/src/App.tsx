678dc4241560565445fb6cba5f0dc4914753e763
prakash <sunnyrkg@gmail.com>
2020-01-25 20:08:47 -0600

    adding ID in food object and adding deleteFoodById

--- nom-nom-ui/src/App.tsx
+++ nom-nom-ui/src/App.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import './App.css';
-import { getUserById, getAllFood, getAllUsers, getFoodById, postUser, postFood, Food, User } from './utils/DataService';
+import { getUserById, getAllFood, getAllUsers, getFoodById, postUser, postFood, Food, User, deleteFoodById } from './utils/DataService';
 import { FoodComponent } from './components/FoodView';
 import { UserComponent } from './components/UserView';
+
 import { isUndefined } from 'util';
 
 
@@ -20,44 +21,45 @@ class App extends React.Component<{}, AppState> {
 
   async componentDidMount() {
 
-    let foodData = {type: "chinese",
-      servings: "4",
-      location: {Latitude: 20,
-        Longitude: 30},
-      address: "123 Second Street Columbia TN",
-      prepDate: new Date(),
-      allergens: [],
-      name:"karuna",
-      number:"111-11-1111",
-      email:"test@test.com"
-    };
+    // let foodData = {type: "chinese",
+    //   servings: "4",
+    //   location: {Latitude: 20,
+    //     Longitude: 30},
+    //   address: "123 Second Street Columbia TN",
+    //   prepDate: new Date(),
+    //   allergens: [],
+    //   name:"karuna",
+    //   number:"111-11-1111",
+    //   email:"test@test.com",
+    //   id: undefined
+    // };
 
-      await postFood(foodData);
+    //   await postFood(foodData);
 
-    let userData = {name: "Heena", number:"6154561234", email:"test@test.com"};
-    await postUser(userData);
-
-    const foodById = await getFoodById('5kAI1oX8SJjnXVOz3x2i');
-    // console.table(foodById);
-    console.log(foodById);
+    
+    const foodById = await getFoodById('RVvnUn396zVVHzFrZxOu');
+     console.table(foodById);
+    //console.log(foodById);
     // const userById = await getUserById('PNzJhGWDcyMeU2CZPUMn');
     // //console.table(userById);
     // console.log(userById);
-    this.setState({
-      foodItem: foodById,
-      userItem: undefined
-    });
+    // this.setState({
+    //   foodItem: foodById,
+    //   userItem: undefined
+    // });
+
+    await deleteFoodById(foodById.id as string);
+
   }
 
   render() {
-    const foodItemElement = this.state.foodItem ? <FoodComponent food={this.state.foodItem}></FoodComponent> : 'Loading...';
-  //  const userItemElement = this.state.userItem ? <UserComponent user={this.state.userItem}></UserComponent> : 'Loading...';
-
+    // const foodItemElement = this.state.foodItem ? <FoodComponent food={this.state.foodItem}></FoodComponent> : 'Loading...';
+  
     return <div>
       <h1>Food Items:</h1>
-      {foodItemElement}
-      {/* <h1>User Items:</h1>
-      {userItemElement} */}
+      {/* {foodItemElement} */}
+      {/* <h1>Guest Form:</h1>
+      <GuestForm></GuestForm> */}
     </div>;
   }
 }
