4e620607f786413697c18b7d984deeccff7aa816
micahhauge <micahhauge@gmail.com>
2020-01-25 22:35:33 -0600

    clearnup

--- nom-nom-ui/src/App.tsx
+++ nom-nom-ui/src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import { getAllFood, Food } from './utils/DataService';
 import { ListFoodComponent } from './components/ListFoodComponent';
 import FoodForm from './components/FoodForm';
